d0cb271b81ef6e4dc37825f72c24a733b4ac9c5b
Aquarius (Research center) <pmamontov@aq.ru>
2018-05-24 11:06:47 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! This is A app. Web Hook is OK!"
+    return "Hello World! This is A app."
 
 if __name__ == "__main__":
     application.run()
