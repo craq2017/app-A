e3da1076089a25749d2f682dad48d58ff5b6a1c6
Aquarius (Research center) <pmamontov@aq.ru>
2018-11-16 16:10:32 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World! This is A app YAY!!! DEVOPSDAYS MOSCOW!!!"
+    return "Hello World! This is A app YAY!!! CLOUDNATIVE ROADSHOW MOSCOW!!!"
 
 if __name__ == "__main__":
     application.run()
